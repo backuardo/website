dc1c201a171c0dba56557044c10972de52dbf059
backuardo <benjaminaeisner@gmail.com>
2025-02-23 06:56:46 -0500

    initial commit

---
+++ app/workbench/page.tsx
@@ -0,0 +1,17 @@
+import { BlogPosts } from "app/components/posts";
+
+export const metadata = {
+  title: "workbench",
+  description: "",
+};
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        Workbench
+      </h1>
+      <BlogPosts />
+    </section>
+  );
+}
