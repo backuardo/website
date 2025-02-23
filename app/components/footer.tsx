ac172365f1aeeb468f19432f581cb687e7e2b65d
backuardo <benjaminaeisner@gmail.com>
2025-02-23 08:30:16 -0500

    remove dark mode

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300 uppercase">
+      <ul className="mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 uppercase">
         <li>
           <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100 text-xs"
+            className="flex items-center transition-all hover:text-neutral-800 text-xs"
             rel="noopener noreferrer"
             target="_blank"
             href="/rss"
@@ -14,7 +14,7 @@ export default function Footer() {
         </li>
         <li>
           <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100 text-xs"
+            className="flex items-center transition-all hover:text-neutral-800 text-xs"
             rel="noopener noreferrer"
             target="_blank"
             href="https://github.com/backuardo"
@@ -24,7 +24,7 @@ export default function Footer() {
         </li>
         <li>
           <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100 text-xs"
+            className="flex items-center transition-all hover:text-neutral-800 text-xs"
             rel="noopener noreferrer"
             target="_blank"
             href="https://github.com/backuardo/website"
@@ -33,7 +33,7 @@ export default function Footer() {
           </a>
         </li>
       </ul>
-      <p className="mt-2 text-neutral-600 dark:text-neutral-300 text-xs">
+      <p className="mt-2 text-neutral-600 text-xs">
         © {new Date().getFullYear()} Backuardo Labs
       </p>
     </footer>
