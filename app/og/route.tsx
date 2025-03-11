e377e79842547e1e8692cc59a8eaed172e0d0a90
backuardo <benjaminaeisner@gmail.com>
2025-03-11 09:28:32 -0400

    og

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,17 +1,11 @@
+import { Logo } from "app/components/logo";
 import { ImageResponse } from "next/og";
 
 export function GET(request: Request) {
-  let url = new URL(request.url);
-  let title = url.searchParams.get("title") || "beneisner.io";
-
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
-            {title}
-          </h2>
-        </div>
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-neutral-900">
+        <Logo width={420} height={500} stroke="#b8bb26" fill="#b8bb26" />
       </div>
     ),
     {
