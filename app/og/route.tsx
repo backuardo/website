0d657b59ba1b1a381c502d7f89c53f331583bec1
backuardo <benjaminaeisner@gmail.com>
2025-03-14 06:25:58 -0400

    add page title to OG

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -2,6 +2,8 @@ import { Logo } from "app/components/logo";
 import { ImageResponse } from "next/og";
 
 export function GET(request: Request) {
+  // const url = new URL(request.url);
+  // const title = url.searchParams.get("title") || "beneisner.io";
   return new ImageResponse(
     (
       <div tw="flex flex-col w-full h-full items-center justify-center bg-neutral-900">
